69e4f0236a5984c259187fb85f226d71fab0ae14
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-12 03:28:13 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -14,4 +14,8 @@ class Board
   def display
    puts output = " X | X | X ","-----------"," X | O | O "," X | O | X "," O | X | X "," O | X | O "
   end
+  
+  def position
+    input = gets.chomp
+  end
 end
