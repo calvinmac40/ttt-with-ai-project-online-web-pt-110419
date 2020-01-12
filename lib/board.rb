dbe8ecc4cb43f16fe6f2955d1106546a03c6fc31
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-12 03:03:06 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -13,6 +13,7 @@ class Board
   
   def display
     puts "#{@cells[0]} | #{@cells[1]} | #{@cells[2]}"
+         "----------"
     puts "#{@cells[3]} | #{@cells[4]} | #{@cells[5]}"
     puts "#{@cells[6]} | #{@cells[7]} | #{@cells[8]}"
   end
