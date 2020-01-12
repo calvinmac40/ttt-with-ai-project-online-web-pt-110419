ddf4ba4a3db22d561e7f05e20167364e9da2a091
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-12 03:33:17 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -13,10 +13,10 @@ class Board
   
   def display
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
    #puts output = " X | X | X ","-----------"," X | O | O "," X | O | X "," O | X | X "," O | X | O "
   end
   
